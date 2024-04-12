cf9ce876323ece7b8c3354482ee872685ed01330
Micaela Cuadrado <micaela.cuadrado@anima.edu.uy>
2024-04-12 18:28:06 -0300

    Updated adminSeeders.js and runAllSeeders.js

--- seeders/adminSeeders.js
+++ seeders/adminSeeders.js
@@ -5,14 +5,13 @@ async function adminSeeder() {
   for (let i = 0; i < 5; i++) {
     const firstname = faker.person.firstName();
     const lastname = faker.person.lastName();
-    const newUser = {
+    const newAdmin = {
       firstname,
       lastname,
       email: faker.internet.email({ firstName: firstname, lastName: lastname }),
-      phoneNumber: faker.phone.number(),
       password: "1234",
     };
-    await Admin.create(newUser);
+    await Admin.create(newAdmin);
   }
 }
 
--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const userSeeder = require("./userSeeders");
+const adminSeeder = require("./adminSeeders");
 
 userSeeder();
+adminSeeder();
 console.log("Se corrieron los seeders!");
