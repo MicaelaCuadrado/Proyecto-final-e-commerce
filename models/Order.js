61a79b522f37a3a8e6f80073912f70f4449ee279
ValentinaMernies <valentina.mernies@anima.edu.uy>
2024-04-11 21:04:13 -0300

    Created category and order
    Created category and order

---
+++ models/Order.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Order extends Model {
+  static initModel(sequelize) {
+    Order.init(
+      {
+        id: {
+          type: DataTypes.BIGINT,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        items: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        status: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        : {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "orders",
+      }
+    );
+    return Order;
+  }
+}
+
+module.exports = Order;
