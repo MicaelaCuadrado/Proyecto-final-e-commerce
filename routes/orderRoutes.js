a7b6ced1b2e8703dd58bdfd9d334e9615fac4d4d
Valentina Mernies <valentina.mernies@anima.edu.uy>
2024-04-16 19:08:25 -0300

    orderRouter.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,1 +1,11 @@
-const expres = require("express");
+const express = require("express");
+const router = express.Router();
+const orderController = require("../controllers/orderController");
+
+router.get("/", orderController.index);
+router.get("/:id", orderController.show);
+router.post("/", orderController.store);
+router.patch("/:id", orderController.update);
+router.delete("/:id", orderController.destroy);
+
+module.exports = router;
