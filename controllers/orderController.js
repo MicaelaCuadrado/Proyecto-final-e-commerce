a352c02aa1a44483b5d45628eeaa8c8c8fe04bb8
Valentina Mernies <valentina.mernies@anima.edu.uy>
2024-04-16 20:18:29 -0300

    update orderControllers

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,18 +1,37 @@
-store: async (req, res) => {
-  try {
+const { Order, User } = require("../models");
+const { destroy } = require("../models/Admin");
+const ordersController = {
+  index: async (req, res) => {
+    try {
+      const orders = await Order.findAll({ include: User });
+      return res.status(200).json(orders);
+    } catch (error) {
+      console.error(err);
+      return res.status(500).json({ message: "Oops, something went wrong" });
+    }
+  },
+
+  show: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const order = await Order.findByPk(id);
+      if (!order) {
+        return res.status(404).json({ error: "Order not found" });
+      }
+      return res.json;
+    } catch (error) {
+      return res.status(500).json({ error: "Error getting order" });
+    }
+  },
+  store: async (req, res) => {
     const order = req.body;
 
-    //clausulas de guarda
-
-    if (!order.address) return res.json({ message: "Ups! Algo salió mal." });
-    if (!order.userId) return res.json({ message: "Ups! Algo salió mal." });
-    // recorremos los productos que vienen en la orden via req.body y cortamos la funcion si el stock es insuficiente, y agregamos losmprecios sacamos de la db. (data base)
     for (const product of order.products) {
       // TODO : obtener userId de forma segura
       const productInDb = await Product.findByPk(product.id);
       if (productInDb.stock < product.qty) {
         return res.json({
-          message: "Unsuficient stock.",
+          message: "Insiuficiente stock",
           product: product.id,
           stock: productInDb.stock,
         });
@@ -30,9 +49,36 @@ store: async (req, res) => {
     }
 
     await Order.create(order);
-    return res.json({ message: "Orden recibida." });
-  } catch (err) {
-    console.error(err);
-    return res.json({ message: "Ups! Algo salió mal." });
+    return res.json({ message: "Orden recibida" });
+  },
+};
+
+update: async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  try {
+    const order = await Order.findByPk(id);
+    if (!orden) {
+      return res.status(404).json({ error: "Orden no encontrada" });
+    }
+    await order.save();
+    return res.send("Orden modificada con exito");
+  } catch (error) {
+    return res.status(500).json({ error: "Error en modificar orden" });
   }
 };
+destroy: async (req, res) => {
+  const { id } = req.params;
+  try {
+    const order = await Order.findByPk(id);
+    if (!order) {
+      return res.status(404).json({ error: "Orden no encontrada" });
+    }
+    await order.destroy();
+    return res.json({ message: "Orden eliminada con éxito" });
+  } catch (error) {
+    return res.status(500).json({ error: "Error eliminando la órden" });
+  }
+};
+
+module.exports = ordersController;
--- models/Order.js
+++ models/Order.js
@@ -16,6 +16,7 @@ class Order extends Model {
         status: {
           type: DataTypes.STRING,
           allowNull: false,
+          values: ["pending", ]
         },
       },
       {
