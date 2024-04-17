803cc770291e992880710e929f8135995b56a4d0
Micaela Cuadrado <micaela.cuadrado@anima.edu.uy>
2024-04-17 19:42:08 -0300

    Updated runAllSeeders.js

--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 
 const userSeeder = require("./userSeeders");
 const adminSeeder = require("./adminSeeders");
+const orderSeeder = require("./orderSeeders");
+const productSeeder = require("./productsSeeders");
 
 userSeeder();
 adminSeeder();
+orderSeeder();
+productSeeder;
 console.log("Se corrieron los seeders!");
