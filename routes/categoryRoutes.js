0c0f1a49260c2f48533b8bf256174f594bfc31c8
Micaela Cuadrado <micaela.cuadrado@anima.edu.uy>
2024-04-17 19:45:27 -0300

    Updated category routes and created category seeders

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const categoryController = require("../controllers/categoryController");
+
+router.get("/", categoryController.index);
+
+router.get("/:id", categoryController.show);
+
+router.post("/", categoryController.store);
+
+router.patch("/:id", categoryController.update);
+
+router.delete("/:id", categoryController.destroy);
+
+module.exports = router;
