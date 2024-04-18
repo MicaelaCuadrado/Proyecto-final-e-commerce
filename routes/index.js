32c810aed4a9ec638dbac6a167c436ea631a9633
Valentina Mernies <valentina.mernies@anima.edu.uy>
2024-04-18 19:12:59 -0300

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const adminRoutes = require("./adminRoutes");
-const authRoutes=require("./authRoutes");
-const userRoutes=require("./userRoutes")
+const authRoutes = require("./authRoutes");
+const userRoutes = require("./userRoutes");
+const orderRoutes = require("./orderRoutes");
+const categoryRoutes = "./categoryRoutes";
 
-
+router.use(express.json());
 
 router.use("/admins", adminRoutes);
 router.use("/tokens", authRoutes);
 router.use("/user", userRoutes);
+router.use("/orders", orderRoutes);
+router.use("/categories", categoryRoutes);
 
 module.exports = router;
