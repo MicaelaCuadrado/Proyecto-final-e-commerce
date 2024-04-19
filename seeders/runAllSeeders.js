a508e02f271fd22eacb1597794ea4a8fdfb11566
Valentina Mernies <valentina.mernies@anima.edu.uy>
2024-04-18 21:14:50 -0300

    Added categorySeeder to runAllSeeders

--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -4,9 +4,11 @@ const userSeeder = require("./userSeeders");
 const adminSeeder = require("./adminSeeders");
 const orderSeeder = require("./orderSeeders");
 const productSeeder = require("./productsSeeders");
+const categorySeeder = require("./categorySeeders");
 
 userSeeder();
 adminSeeder();
 orderSeeder();
 productSeeder();
+categorySeeder();
 console.log("Se corrieron los seeders!");
