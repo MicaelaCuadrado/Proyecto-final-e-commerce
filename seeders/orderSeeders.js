68b804a6e78de03967df652447b654f8670ff356
Rocio Alvarez <rocio.alvarez@anima.edu.y>
2024-04-24 20:48:04 -0300

    Fixed order seeders.
    Some fields were not required.

--- seeders/orderSeeders.js
+++ seeders/orderSeeders.js
@@ -9,46 +9,29 @@ async function orderSeeder() {
       items: [
         {
           name: "Musculosa Gris",
-          
           price: 690,
           stock: 25,
         },
-       /*  {
+        {
           name: "Remera Negra",
-          description:
-            "Remera de poliester negra. Lavable a maquina. Ideal para entrenar.",
           price: 720,
           stock: 15,
-          categoryId: "PENDIENTE",
-          featured: false,
         },
         {
           name: "Remera Celeste",
-          description:
-            "Remera de algodón celeste. Lavable a maquina. Ideal para entrenar.",
           price: 720,
           stock: 15,
-          categoryId: "PENDIENTE",
-          featured: false,
         },
         {
           name: "Remera Naranja",
-          description:
-            "Remera de algodón naranja. Lavable a maquina. Ideal para entrenar.",
           price: 500,
           stock: 10,
-          categoryId: "PENDIENTE",
-          featured: true,
         },
         {
           name: "Bandas Elásticas de Resistencia",
-          description:
-            "Este pack contiene 4 bandas de latex en 4 niveles de resistencia: ligero, medio, pesado.",
           price: 549,
           stock: 20,
-          categoryId: "PENDIENTE",
-          featured: false,
-        }, */
+        },
       ],
     };
     await Order.create(newOrder);
