aac7eac3cccf4f0ca7340524f1ebb22e2e75ee1f
Micaela Cuadrado <micaela.cuadrado@anima.edu.uy>
2024-04-25 20:43:13 -0300

    Updated auth Controller
    Generate token rutes for admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("../models");
+const { User, Admin } = require("../models");
 require("dotenv").config();
 const bcrypt = require("bcryptjs");
 
@@ -8,15 +8,29 @@ const authController = {
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
+    const admin = await Admin.findOne({ where: { email } });
+    console.log(user.password);
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!(admin && (await bcrypt.compare(password, admin.password)))) {
+      if (!(user && (await bcrypt.compare(password, user.password)))) {
+        return res.json({ message: "Credenciales invalidas" });
+      }
+    }
+    if (admin) {
+      const accessToken = jwt.sign(
+        { sub: admin.id, role: "admin" },
+        process.env.SECRET
+      );
 
-    if (!passwordMatch)
-      return res.json({ message: "Credenciales invalidas" });
+      return res.json(accessToken);
+    } else if (user) {
+      const accessToken = jwt.sign(
+        { sub: user.id, role: "user" },
+        process.env.SECRET
+      );
 
-    const accessToken = jwt.sign({ sub: user.id }, process.env.SECRET);
-
-    return res.json(accessToken);
+      return res.json(accessToken);
+    }
   },
 };
 module.exports = authController;
