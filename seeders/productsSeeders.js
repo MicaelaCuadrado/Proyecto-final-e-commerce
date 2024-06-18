a5959856668c45d5902dde9e2f0382203f95a465
Micaela Cuadrado <micaela.cuadrado@anima.edu.uy>
2024-06-18 19:08:59 -0300

    Updated seeders products

--- seeders/productsSeeders.js
+++ seeders/productsSeeders.js
@@ -3,60 +3,62 @@ const { Products } = require("../models");
 async function productsSeeder() {
   const products = [
     {
-      name: "Disco Olimpico de Goma 10kg",
+      name: "Disco olímpico crossfit 10kg",
       description:
-        "El pase central esta hecho de acero inoxidable, permite la colocación sencilla de la barra.",
-      price: 690,
+        "El pase central es de acero inoxidable, ésto permite una colocación sencilla de la barra.",
+      price: 4290,
       stock: 25,
       categoryId: 1,
       featured: false,
       photo: "Disco10.png",
     },
     {
-      name: "Disco Olimpico de Goma 15kg",
+      name: "Disco olímpico de goma 15kg",
       description:
-        "El pase central esta hecho de acero inoxidable, permite la colocación sencilla de la barra.",
-      price: 3.89,
+        "El pase central es de acero inoxidable, ésto permite la colocación sencilla de la barra.",
+      price: 6290,
       stock: 25,
       categoryId: 1,
       featured: false,
       photo: "Disco15.jpg",
     },
     {
-      name: "Disco Olimpico de Goma 20kg",
+      name: "Disco olímpico de goma 20kg",
       description:
-        "El pase central esta hecho de acero inoxidable, permite la colocación sencilla de la barra.",
-      price: 3.89,
+        "El pase central es de acero inoxidable, ésto permite la colocación sencilla de la barra.",
+      price: 7190,
       stock: 15,
       categoryId: 1,
       featured: false,
       photo: "Disco20.png",
     },
     {
-      name: "Step para Fitness",
+      name: "Step para fitness",
       description:
-        "Step de aeróbic profesional ideal para hacer ejercicios en casa o en el gimnasio. Ayuda a definir la musculatura, mejorar la flexibilidad y la estabilidad, y también mejora la resistencia cardiovascular",
-      price: 890,
+        "Step de aeróbic profesional, ideal para hacer ejercicios en casa o en el gimnasio. Ayuda a definir la musculatura, mejorar la flexibilidad y la estabilidad, tendrás una notable mejora en la resistencia cardiovascular",
+      price: 1390,
       stock: 15,
       categoryId: 1,
       featured: false,
       photo: "Step.png",
     },
+
     {
-      name: "Rueda Abdominal",
+      name: "Rueda abdominales",
       description:
-        "La secuencia del movimiento está optimizado para trabajar todo el abdomen, pero además también los músculos de apoyo en los brazos, la parte posterior de las piernas, los hombros y la espalda sin correr el riesgo de dolor o incluso lesiones. Además, esta rueda cuenta con pedales que permiten más ejercicios para el abdomen, la espalda y las piernas.",
-      price: 499,
+        "La secuencia de movimientos está diseñada para trabajar todo el abdomen y también los músculos de apoyo en brazos, piernas posteriores, hombros y espalda, minimizando el riesgo de dolor o lesiones. Además, la rueda incluye pedales que permiten realizar más ejercicios para el abdomen, la espalda y las piernas. ",
+      price: 397,
       stock: 10,
       categoryId: 1,
       featured: false,
       photo: "RuedaAbdominal.png",
     },
+
     {
-      name: "Bandas Elásticas de Resistencia",
+      name: "Bandas elásticas de resistencia",
       description:
-        "Este pack contiene 4 bandas de latex en 4 niveles de resistencia: ligero, medio, pesado.",
-      price: 549,
+        "Set 3 bandas de latex. Incluye 3 niveles de resistencia: ligero, medio y pesado.",
+      price: 499,
       stock: 20,
       categoryId: 1,
       featured: true,
@@ -66,7 +68,7 @@ async function productsSeeder() {
       name: "Mancuernas de 1kg",
       description:
         "Mancuernas revestidas en vinilo. Hechas de hierro fundido de alta calidad.",
-      price: 210,
+      price: 249,
       stock: 15,
       categoryId: 1,
       featured: false,
@@ -76,7 +78,7 @@ async function productsSeeder() {
       name: "Mancuernas de 2kg",
       description:
         "Mancuernas revestidas en vinilo. Hechas de hierro fundido de alta calidad.",
-      price: 300,
+      price: 549,
       stock: 15,
       categoryId: 1,
       featured: false,
@@ -86,7 +88,7 @@ async function productsSeeder() {
       name: "Mancuernas de 3kg",
       description:
         "Mancuernas revestidas en vinilo. Hechas de hierro fundido de alta calidad.",
-      price: 399,
+      price: 789,
       stock: 15,
       categoryId: 1,
       featured: false,
@@ -96,7 +98,7 @@ async function productsSeeder() {
       name: "Mancuernas de 5kg",
       description:
         "Mancuernas revestidas en vinilo. Hechas de hierro fundido de alta calidad.",
-      price: 500,
+      price: 1090,
       stock: 15,
       categoryId: 1,
       featured: true,
@@ -106,7 +108,7 @@ async function productsSeeder() {
       name: "Cuerda de saltar",
       description:
         "Ligera, ajustable y duradera, es ideal para mejorar tu resistencia y agilidad. ¡Hazte con ella y eleva tu rutina de ejercicio!",
-      price: 499,
+      price: 549,
       stock: 20,
       categoryId: 1,
       featured: false,
@@ -116,7 +118,7 @@ async function productsSeeder() {
       name: "Cuerda de trepar",
       description:
         "Nuestra cuerda de escalada es fabricada con sisal resistente. Tiene una longitud de 5 metros y un diámetro de 5.0cm. Es perfecta para Crossfit y otros ejercicios de entrenamiento.",
-      price: 2000,
+      price: 4190,
       stock: 5,
       categoryId: 1,
       featured: false,
@@ -133,10 +135,10 @@ async function productsSeeder() {
       photo: "colchonetaYoga.png",
     },
     {
-      name: "Pelota Medicinal de Yoga",
+      name: "Pelota de Pilates",
       description:
         "Pelota de yoga de PVC. Incluye 1 tapón de aire, 1 tapón de aire de repuesto, Inflador",
-      price: 890,
+      price: 990,
       stock: 10,
       categoryId: 1,
       featured: false,
@@ -146,7 +148,7 @@ async function productsSeeder() {
       name: "Whey Protein 1kg",
       description:
         "Este es un suplemento proteico-energético para deportistas a base de Whey Protein y Maltodextrina.",
-      price: 1999,
+      price: 2290,
       stock: 15,
       categoryId: 2,
       featured: false,
@@ -156,34 +158,34 @@ async function productsSeeder() {
       name: "Whey Protein 2kg Chocolate",
       description:
         "Es una combinación ultra premium del Isolado, Concentrado e Hidrolizado de proteína de suero de la más alta calidad para un desarrollo muscular óptimo, soporte y recuperación.",
-      price: 4800,
+      price: 3590,
       stock: 10,
       categoryId: 2,
       featured: false,
       photo: "WheyProteinChoc.jpg",
     },
     {
-      name: "Guantes Musculación Negro",
+      name: "Guantes musculación negro",
       description:
         "Excelentes guantes de entrenamiento. La almohadilla de la palma amortigua el impacto del aparato deportivo y te protege las manos de callos y ampollas.",
-      price: 390,
+      price: 890,
       stock: 15,
       categoryId: 1,
       featured: false,
       photo: "GuantesNeopreno.png",
     },
     {
-      name: "Rodilleras de Neopreno",
+      name: "Rodilleras de neopreno",
       description:
         "Hechas de tela de neopreno de 7mm y costuras reforzadas hacen que nuestra rodillera sea resistente y duradera. Obtendrás seguridad y fuerza durante los entrenamientos de alta intensidad.",
-      price: 1690,
+      price: 1390,
       stock: 5,
       categoryId: 1,
       featured: true,
       photo: "rodillera.jpg",
     },
     {
-      name: "Botella Agua 1lt",
+      name: "Botella agua 1lt",
       description:
         "Hecha de Tritán de alta calidad, reutilizable y 100% libre de BPA. Disfrutá la durabilidad y la facilidad de una botella libre de sabores y olores a plástico.",
       price: 499,
